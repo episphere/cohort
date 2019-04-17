45968b027ea6096705b3275bdc1743a334870e3c
Jonas Almeida <jalmeida@mathbiol.org>
2019-04-17 14:50:40 -0400

    dev localhost

--- cohort.js
+++ cohort.js
@@ -131,11 +131,11 @@ cohort.boxdrive=async function(div){
     if(!cohort.boxdrive.config){
         cohort.boxdrive.config={}
         if(location.hostname=="localhost"){
-            cohort.boxdrive.config.client_id='chjgzcmmrvkeezw0ausf9up81oyzsf8m'
-            cohort.boxdrive.config.server_id='aX6RT5waUHQ2uipbVFNYyf0oB6eQT9dY' //<-- reset when proxy is deployed
+            cohort.boxdrive.config.client_id='52zad6jrv5v52mn1hfy1vsjtr9jn5o1w'
+            cohort.boxdrive.config.server_id='2rHTqzJumz8s9bAjmKMV83WHX1ooN4kT' //<-- reset when proxy is deployed
         }else{
-            cohort.boxdrive.config.client_id='chjgzcmmrvkeezw0ausf9up81oyzsf8m'
-            cohort.boxdrive.config.server_id='aX6RT5waUHQ2uipbVFNYyf0oB6eQT9dY' //<-- reset when proxy is deployed
+            cohort.boxdrive.config.client_id='1n44fu5yu1l547f2n2fgcw7vhps7kvuw'
+            cohort.boxdrive.config.server_id='2ZYzmHXGyzBcjZ9d1Ttsc1d258LiGGVd' //<-- reset when proxy is deployed
         }
     }
     cohort.getParms() // <-- getting parameters here!
